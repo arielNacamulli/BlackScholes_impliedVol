ad3b9471d4c88f27e6c2f045fc679025d21cc9a1
arielNacamulli <57598969+arielNacamulli@users.noreply.github.com>
2019-12-09 16:30:23 +0100

    Update VolSmile.cpp

--- VolSmile.cpp
+++ VolSmile.cpp
@@ -34,3 +34,26 @@ void VolSmile::calibrate() {
     spline();
     coeff = getCoeff();
 }
+
+void VolSmile::read(const string &file) {
+
+    double tempStrike,tempPrice;
+    int tempFlag;
+    std::ifstream ist{file};
+    if(!ist)
+        cout << "Can't open the file!" << endl;
+    while(ist >> tempStrike >> tempPrice >> tempFlag)
+        newSpot(tempStrike,tempPrice,tempFlag);
+}
+
+void VolSmile::write(const string &file, int len) const {
+    std::ofstream ost{file};
+    vector<double> KVec = linspace(x.front(),x.back(),len);
+    vector<double> curve(len);
+
+    for(size_t i = 0; i < len; i++)
+        curve[i] = interpolate(KVec[i]);
+
+    for(size_t i = 0; i < len; i++)
+        ost << KVec[i] << ' ' << curve[i] << endl;
+}
